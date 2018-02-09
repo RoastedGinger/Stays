fab2ae58cd264d5d44d9d734778198579f1a862b
pritz <pritzzzzz101010@gmail.com>
2018-02-09 19:56:04 +0530

    payment need to add now

--- app/src/main/java/com/handshake/pritz/organichome/Main2Activity.java
+++ app/src/main/java/com/handshake/pritz/organichome/Main2Activity.java
@@ -1,11 +1,13 @@
 package com.handshake.pritz.organichome;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -27,7 +32,11 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class Main2Activity extends AppCompatActivity {
-Button b,del,update;
+Button b,gh,del,update,cncl,cnclbtn,delbtn;
+EditText newPrice;
+    AlertDialog.Builder builder=null;
+    AlertDialog dialog=null;
+    ProgressDialog progressDialog;
 TextView hname,haddress,hprice,load;
 ProgressBar progressBar;
 DatabaseReference mref;
@@ -51,7 +60,32 @@ String aname,aaddress,aimage,aprice;
              root = bundle.getString("keyroot");
             mref = FirebaseDatabase.getInstance().getReference().child(root);
         }
-         progressBar=findViewById(R.id.progressBar);
+        authStateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null)
+                {del.setVisibility(View.VISIBLE);
+                del.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        deluser();
+                    }
+                });
+                update.setVisibility(View.VISIBLE);
+                update.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AddUser();
+                    }
+                });
+
+                }
+
+            }
+        };
+        progressDialog = new ProgressDialog(this);
+        progressBar=findViewById(R.id.progressBar);
         b=findViewById(R.id.book);
         del=findViewById(R.id.del);
         update=findViewById(R.id.update);
@@ -102,20 +136,7 @@ String aname,aaddress,aimage,aprice;
 
             }
         });
-        authStateListener=new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser()!=null)
-                {
-                    FirebaseUser currentUser = mauth.getCurrentUser();
-                    if (currentUser.getUid().equals(postid)) {
-                        del.setVisibility(View.VISIBLE);
-                        update.setVisibility(View.VISIBLE);
-                    }
-                }
 
-            }
-        };
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -150,5 +171,105 @@ del.setOnClickListener(new View.OnClickListener() {
 
 
         }
+    public void AddUser()
+    {   LayoutInflater linf=LayoutInflater.from(Main2Activity.this);
+        View inflator=linf.inflate(R.layout.homeupdate,null);
+        newPrice=inflator.findViewById(R.id.new_price);
+        newPrice.setText(aprice);
+        gh=inflator.findViewById(R.id.gh);
+        cncl=inflator.findViewById(R.id.cncl);
+        builder=new AlertDialog.Builder(Main2Activity.this);
+    //    builder.setTitle("Update Price...");
+        builder.setView(inflator);
+        dialog=builder.create();
+        dialog.show();
+        cncl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+                dialog.dismiss();
+            }
+        });
+        gh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status = newPrice.getText().toString();
+               progressDialog.setMessage("Updating...");
+                progressDialog.show();
+                //progressDialog.setCancelable(false);
+                mref.child(message).child("Price").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                            progressDialog.dismiss();
+                            dialog.dismiss();
+                            dialog.cancel();
+                            Toast.makeText(Main2Activity.this, "Successfully Updated...", Toast.LENGTH_LONG).show();
+
+
+                        } else {
+
+                            Toast.makeText(Main2Activity.this, "There was some error in saving Changes.", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+     }
+    public void deluser()
+    {   LayoutInflater linf=LayoutInflater.from(Main2Activity.this);
+        View inflator=linf.inflate(R.layout.homedelete,null);
+        newPrice=inflator.findViewById(R.id.new_price);
+        delbtn=inflator.findViewById(R.id.delbtn);
+        cnclbtn=inflator.findViewById(R.id.cnclbtn);
+        cnclbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                dialog.cancel();
+            }
+        });
+
+        builder=new AlertDialog.Builder(Main2Activity.this);
+        //    builder.setTitle("Update Price...");
+        builder.setView(inflator);
+        dialog=builder.create();
+
+        dialog.show();
+        delbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.setMessage("Deleting HomeStays...");
+                progressDialog.show();
+                //progressDialog.setCancelable(false);
+                mref.child(message).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                            progressDialog.dismiss();
+                            dialog.dismiss();
+                            dialog.cancel();
+                            Toast.makeText(Main2Activity.this, "Successfully Deleted...", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                            finish();
+                            startActivity(intent);
+
+
+
+                        } else {
+
+                            Toast.makeText(Main2Activity.this, "There was some error in saving Changes.", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+    }
      }
 
